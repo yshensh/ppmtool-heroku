0684c835fbb917b36de05fc56f2dd918560a4df0
Yi Shen <yshensh@gmail.com>
2020-07-20 17:31:19 -0400

    add frontend

---
+++ src/main/resources/static/precache-manifest.4ca6a6bb5969ebea2bffffaecafbd283.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5054bc4bdb0675757153bccb56c2b5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7ee413828809b8bffc5",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "8151c52d563210c4b923",
+    "url": "/static/css/main.d242b985.chunk.css"
+  },
+  {
+    "revision": "d7ee413828809b8bffc5",
+    "url": "/static/js/2.6c2f76a0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6c2f76a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8151c52d563210c4b923",
+    "url": "/static/js/main.0a51c837.chunk.js"
+  },
+  {
+    "revision": "aa13666c62c3b811923f",
+    "url": "/static/js/runtime-main.523a9cbe.js"
+  }
+]);
